b4deb677439145e457d88dba36f6df6c56e22894
FattyRatty <60388760+emilasbringer@users.noreply.github.com>
2021-08-05 19:22:21 +0200

    v

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+const gamewindow = document.querySelector("#gamewindow");
+var map = document.querySelector("#map");
+
 var gamestate = 0;
 var fps = 30;
 var tickspeed = 1000/fps;
@@ -6,12 +9,36 @@ var usernameInput;
 var passwordInput;
 var username = "";
 var password = "";
-var xytilemap = [x = [], y = [], tiletype = []];
-var grasstile = document.createElement("tilegrass");
-grasstile.setAttribute("src", "../assets/grasstile.png")
-
-
 var tick = 0;
+
+const directions = {
+    up: "up",
+    down: "down",
+    left: "left",
+    right: "right",
+ }
+
+const keys = {
+    38: directions.up,
+    37: directions.left,
+    39: directions.right,
+    40: directions.down,
+ }
+ 
+ document.addEventListener("keydown", (e) => {
+    var dir = keys[e.which];
+    if (dir && held_directions.indexOf(dir) === -1) {
+       held_directions.unshift(dir)
+    }
+ })
+ 
+ document.addEventListener("keyup", (e) => {
+    var dir = keys[e.which];
+    var index = held_directions.indexOf(dir);
+    if (index > -1) {
+       held_directions.splice(index, 1)
+    }
+ });
 
 if(gamestate == 0) {    
     document.createElement("titlediv");
@@ -31,4 +58,18 @@ function checklogin() {
         isRunning = true;
     }
 }
+
+function initilize() {
+    console.log("initilizing game!")
+    initilizeElements()
+}
+
+function initilizeElements() {
+    map.style.display = "block";
+    var player = document.createElement("div");
+    gamewindow.appendChild(player);
+    player.setAttribute("class", "playerapperance")
+}
+
+
 
